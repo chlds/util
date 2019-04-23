34fd67d4c2af6094c333b244f460e9be7cb05f06
Chris <chlds@yahoo.com>
2019-04-23 16:08:05 +0900

    Use a wrapper function to initialise arguments for the recursive function

---
+++ lib/obj/src/cv2_q.c
@@ -0,0 +1,42 @@
+/* **** Notes
+
+Convert to the (unsigned or) signed quad word.
+
+base: Put the leading address for the letters at.
+
+Remarks:
+Use an initialisation wrapper function to
+initialise the arguments for fn. cv2_q_internal e.g., to
+determine the presence or absense of a sign.
+//*/
+
+
+signed(__cdecl cv2_q(signed short(radix), signed long long(*di), signed char(*base))) {
+
+/* **** DATA, BSS and STACK */
+auto signed const(LIMIT) = (3600);
+auto signed short const(HEXA) = (0x10);
+auto signed short const(BINA) = (0x02);
+auto signed char const(MINUS) = ('-');
+
+auto signed(r);
+auto signed short(flag) = (0x00);
+
+/* **** CODE/TEXT */
+if(!base) return(0x00);
+if(radix<(BINA)) radix = (BINA);
+if(HEXA<(radix)) radix = (HEXA);
+
+if(!(MINUS^(*base))) flag++;
+if(*di) *di = (*di^(*di));
+
+r = cv2_q_internal(radix, di, base);
+
+if(!r) return(0x00);
+// else printf("%s%d\n", "r is: ", r);
+
+if(flag) *di = (0x01+(~(*di)));
+// else printf("%s%lld\n", "*di is: ", *di);
+
+return(0x01);
+}
