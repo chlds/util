37443c4ea22097832faee8aa4213c2b3d08b00c5
Chris <chlds@yahoo.com>
2019-06-06 16:58:56 +0900

    Look for the indent or space behind the caret

--- lib/obj/src/cmdln_ctrl_l.c
+++ lib/obj/src/cmdln_ctrl_l.c
@@ -2,6 +2,8 @@
 
 Press <Ctrl-L> to invoke the function.
 
+Look for the indent or space behind the caret.
+
 Remarks:
 Launch on vu.exe
 Refer at incl/cmdln.h and incl/config.h for the CMDLN_STAT structure
@@ -17,8 +19,15 @@ Refer at incl/cmdln.h and incl/config.h for the CMDLN_STAT structure
 signed(__cdecl cmdln_ctrl_l(CMDLN_STAT(*argp))) {
 
 /* **** DATA, BSS and STACK */
+auto signed char(WS) = (' ');
+auto signed char(HT) = ('\t');
+
+// auto signed char(WS) = (0x20);
+// auto signed char(HT) = (0x09);
+
 auto COORD(coord);
 auto signed(i), (r);
+auto signed short(flag);
 
 /* **** CODE/TEXT */
 if(!argp) return(0x00);
@@ -35,7 +44,30 @@ coord.X = ((*argp).csbi.dwCursorPosition.X);
 coord.Y = ((*argp).csbi.dwCursorPosition.Y);
 }
 
-printf("%s", "Ctrl-L;");
+XOR(flag,flag);
+
+r = cmdln_ctrl_b(argp);
+
+if(!r) {
+printf("%s", "<< Error at fn. cmdln_ctrl_b() in fn. cmdln_ctrl_l()");
+return(0x00);
+}
+
+if((*argp).count) {
+if(!(WS^(*((*argp).p)))) {
+OR(flag,0x01);
+}
+if(!flag) {
+if(!(HT^(*((*argp).p)))) {
+}
+else {
+r = cmdln_ctrl_l(argp);
+if(!r) {
+printf("%s", "<< Error at fn. cmdln_ctrl_l()");
+return(0x00);
+}}}}
+
+// printf("%s", "Ctrl-L;");
 
 return(0x01);
 }
