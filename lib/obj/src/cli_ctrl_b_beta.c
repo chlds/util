fb4cce62cae29019732dc0881c6b028c7bfd1612
Chris <chlds@yahoo.com>
2019-12-20 20:00:55 +0900

    Walk ahead

--- lib/obj/src/cli_ctrl_b_beta.c
+++ lib/obj/src/cli_ctrl_b_beta.c
@@ -9,6 +9,7 @@ Refer at util/lib/obj/src/cli_io_beta.c
 */
 
 
+# define CLI_MACRO
 # define CLI_W32
 
 # include <conio.h>
@@ -18,13 +19,52 @@ Refer at util/lib/obj/src/cli_io_beta.c
 
 signed(__cdecl cli_ctrl_b_beta(CLI_W32_STAT(*argp))) {
 
-auto signed char *p;
+/* **** DATA, BSS and STACK */
+auto signed char *cur,*p;
+auto signed long long sll;
 auto signed c,i,r;
 auto signed short flag;
 
+/* **** CODE/TEXT */
 if(!argp) return(0x00);
 
-if(CLI_DBG) printf("%s","<Ctrl-B>");
+if(CLI_DBG_D<(CLI_DBG)) printf("%s","<Ctrl-B>");
+
+p = (*(CLI_INDEX+(R(cur,R(ty,*argp)))));
+
+sll = ((signed long long) p);
+if(!(sll^((signed long long) *(CLI_BASE+(R(base,R(roll,R(ty,*argp)))))))) return(0x01);
+
+while(0x01) {
+--p;
+r = nbytechar(*p);
+if(!r) {
+printf("%s\n","<< Error at fn. nbytechar()");
+return(0x00);
+}
+if(r^(0x80)) break;
+}
+
+r = cli_ctrl_a_beta(argp);
+if(!r) {
+printf("%s\n","<< Error at fn. cli_ctrl_a_beta()");
+return(0x00);
+}
+
+cur = (*(CLI_BASE+(R(base,R(roll,R(ty,*argp))))));
+
+while(0x01) {
+if(cur<(p)) {
+r = cli_col_out_beta(cur,argp);
+if(!r) {
+printf("%s\n","<< Error at fn. cli_col_out_beta()");
+return(0x00);
+}}
+else break;
+cur = (cur+(r));
+}
+
+*(CLI_INDEX+(R(cur,R(ty,*argp)))) = (cur);
 
 return(0x01);
 }
