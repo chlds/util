a8b95f5de06c3f503f8eefbee84e4e3958543ad1
Chris <chlds@yahoo.com>
2019-12-22 20:06:55 +0900

    Copy and paste

--- lib/obj/src/cli_ctrl_p_beta.c
+++ lib/obj/src/cli_ctrl_p_beta.c
@@ -9,6 +9,7 @@ Refer at util/lib/obj/src/cli_io_beta.c
 */
 
 
+# define CLI_MACRO
 # define CLI_W32
 
 # include <conio.h>
@@ -18,13 +19,21 @@ Refer at util/lib/obj/src/cli_io_beta.c
 
 signed(__cdecl cli_ctrl_p_beta(CLI_W32_STAT(*argp))) {
 
+/* **** DATA, BSS and STACK */
 auto signed char *p;
 auto signed c,i,r;
 auto signed short flag;
 
+/* **** CODE/TEXT */
 if(!argp) return(0x00);
 
-if(CLI_DBG) printf("%s","<Ctrl-P>");
+if(CLI_DBG_D<(CLI_DBG)) printf("%s","<Ctrl-P>");
+
+r = cli_paste_beta(argp);
+if(!r) {
+printf("%s\n","<< Error at fn. cli_paste_beta()");
+return(0x00);
+}
 
 return(0x01);
 }
