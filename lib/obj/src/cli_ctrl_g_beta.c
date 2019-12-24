b006af6069da9fef6cdd1da085af558b75c9b611
Chris <chlds@yahoo.com>
2019-12-24 18:55:06 +0900

    Go forward

--- lib/obj/src/cli_ctrl_g_beta.c
+++ lib/obj/src/cli_ctrl_g_beta.c
@@ -9,6 +9,7 @@ Refer at util/lib/obj/src/cli_io_beta.c
 */
 
 
+# define CLI_MACRO
 # define CLI_W32
 
 # include <conio.h>
@@ -18,13 +19,39 @@ Refer at util/lib/obj/src/cli_io_beta.c
 
 signed(__cdecl cli_ctrl_g_beta(CLI_W32_STAT(*argp))) {
 
-auto signed char *p;
-auto signed c,i,r;
-auto signed short flag;
+/* **** DATA, BSS and STACK */
+auto signed char HT = ('\t');
+auto signed char SP = (' ');
 
+auto signed char *p;
+auto signed i,r;
+auto signed short flag;
+auto signed char c;
+
+/* **** CODE/TEXT */
 if(!argp) return(0x00);
 
-if(CLI_DBG) printf("%s","<Ctrl-G>");
+if(CLI_DBG_D<(CLI_DBG)) printf("%s","<Ctrl-G>");
 
-return(0x01);
+c = (**(CLI_INDEX+(R(cur,R(ty,*argp)))));
+if(!c) return(0x01);
+
+flag = (0x00);
+if(!(HT^(c))) flag = (0x01);
+if(!(SP^(c))) flag = (0x01);
+
+r = cli_ctrl_f_beta(argp);
+if(!r) {
+printf("%s\n","<< Error at fn. cli_ctrl_f_beta()");
+return(0x00);
+}
+
+c = (**(CLI_INDEX+(R(cur,R(ty,*argp)))));
+if(!(HT^(c))) return(0x01);
+
+if(flag) {
+if(SP^(c)) return(0x01);
+}
+
+return(0x01+(cli_ctrl_g_beta(argp)));
 }
