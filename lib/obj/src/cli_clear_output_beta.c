a8db9d4f536d4f273162b900f35e466289c1813a
Chris <chlds@yahoo.com>
2020-01-11 23:30:44 +0900

    Currently under construction

---
+++ lib/obj/src/cli_clear_output_beta.c
@@ -0,0 +1,59 @@
+/*
+
+Output characters based on UTF-8 to the console screen.
+
+Remarks:
+Based on fn. cli_output_beta, fn. cli_col_outs_beta, fn. cli_col_out_beta and fn. cli_out
+Return the number of bytes for output characters.
+*/
+
+
+# define CLI_W32
+
+# include <stdio.h>
+# include "../../../incl/config_ty.h"
+
+signed(__cdecl cli_clear_output_beta(signed short(comeback_flag),signed char(*cur),CLI_W32_STAT(*argp))) {
+
+/* **** DATA, BSS and STACK */
+auto CLI_COORD coord;
+
+auto signed char *p;
+auto signed c,i,r;
+
+/* **** CODE/TEXT */
+if(!cur) return(0x00);
+if(!argp) return(0x00);
+
+if(!(*cur)) return(0x00);
+
+if(comeback_flag) {
+r = cli_coord_beta(CLI_IN,&coord,argp);
+if(!r) {
+printf("%s\n","<< Error at fn. cli_coord_beta()");
+return(0x00);
+}}
+
+r = cli_col_outs_beta(cur,argp);
+if(!r) {
+printf("%s\n","<< Error at fn. cli_col_outs_beta()");
+return(0x00);
+}
+
+i = (r);
+
+r = cli_clear_row_beta(0x00/* come back */,argp);
+if(!r) {
+printf("%s\n","<< Error at fn. cli_clear_row_beta()");
+return(0x00);
+}
+
+if(comeback_flag) {
+r = cli_coord_beta(CLI_OUT,&coord,argp);
+if(!r) {
+printf("%s\n","<< Error at fn. cli_coord_beta()");
+return(0x00);
+}}
+
+return(i);
+}
---
+++ lib/obj/src/cli_merge_pages.c
@@ -0,0 +1,76 @@
+/* **** Notes
+
+Merge a page to the index page in a doubly linked list (for general purpose).
+
+Remarks:
+Refer at fn. cli_init_pages, fn. cli_concat_pages, fn. cli_reconcat_pages, fn. cli_bind_pages and fn. cli_unmap_pages.
+Based on a doubly linked list (i.e., not a circular linked list)
+//*/
+
+
+# define CLI_MACRO
+
+# include <stdio.h>
+# include <stdlib.h>
+# include "../../../incl/config_ty.h"
+
+signed(__cdecl cli_merge_pages(CLI_SPOOL(*argp))) {
+
+/* **** DATA, BSS and STACK */
+auto CLI_PAGE *page;
+
+auto signed char *p;
+auto signed i,r;
+auto signed short flag;
+
+/* **** CODE/TEXT */
+if(!argp) return(0x00);
+
+page = R(d,**(CLI_INDEX+(R(page,*argp))));
+if(!page) return(0x01);
+
+if(!(R(d,*page))) {
+*(CLI_LEAD+(R(page,*argp))) = (R(s,*page));
+R(d,*(R(s,*page))) = (0x00);
+}
+else {
+R(d,*(R(s,*page))) = (R(d,*page));
+R(s,*(R(d,*page))) = (R(s,*page));
+}
+
+r = ct(*(CLI_BASE+(R(base,*(R(s,*page))))));
+i = (r);
+r = ct(*(CLI_BASE+(R(base,*page))));
+INC(r);
+ADD(i,r);
+r = (r*(sizeof(signed char)));
+p = (signed char(*)) malloc(r);
+if(!p) {
+printf("%s\n","<< Error at fn. malloc()");
+return(0x00);
+}
+r = concats(p,*(CLI_BASE+(R(base,**(CLI_INDEX+(R(page,*argp)))))),*(CLI_BASE+(R(base,*page))),(void*) 0x00);
+if(!r) {
+printf("%s\n","<< Error at fn. concats()");
+return(0x00);
+}
+if(*(CLI_BASE+(R(base,*(R(s,*page)))))) free(*(CLI_BASE+(R(base,*(R(s,*page))))));
+*(CLI_BASE+(R(base,*(R(s,*page))))) = (p);
+
+r = cli_unmap_snapshots(&(R(history,*page)));
+if(!r) {
+printf("%s\n","<< Error at fn. cli_unmap_snapshots()");
+return(0x00);
+}
+
+r = cli_unmap(CLI_OBJS,R(base,*page));
+if(!r) {
+printf("%s\n","<< Error at fn. cli_unmap()");
+return(0x00);
+}
+
+free(page);
+page = (0x00);
+
+return(0x01);
+}
