f4e5a65a27bfe789452e735171045561d4b97055
Chris <chlds@yahoo.com>
2020-03-30 17:55:05 +0900

    Fix

--- lib/obj/src/cli_ctrl_h_beta.c
+++ lib/obj/src/cli_ctrl_h_beta.c
@@ -21,7 +21,7 @@ signed(__cdecl cli_ctrl_h_beta(CLI_W32_STAT(*argp))) {
 
 /* **** DATA, BSS and STACK */
 auto signed char *p;
-auto signed long long sll;
+auto signed long long ll;
 auto signed c,i,r;
 auto signed short flag;
 
@@ -31,8 +31,29 @@ if(!argp) return(0x00);
 if(CLI_DBG_D<(CLI_DBG)) printf("%s","<Ctrl-H>");
 
 p = (*(CLI_INDEX+(R(cur,R(ty,*argp)))));
-sll = ((signed long long) p);
-if(!(sll^((signed long long) *(CLI_BASE+(R(base,R(roll,R(ty,*argp)))))))) return(0x01);
+ll = ((signed long long) p);
+if(!(ll^((signed long long) *(CLI_BASE+(R(base,R(roll,R(ty,*argp)))))))) {
+//* aux.
+ll = ((signed long long) *(CLI_INDEX+(R(page,R(spool,R(ty,*argp))))));
+if(ll^((signed long long) *(CLI_BASE+(R(page,R(spool,R(ty,*argp))))))) {
+r = cli_ctrl_u_beta(argp);
+if(!r) {
+printf("%s\n","<< Error at fn. cli_ctrl_u_beta()");
+return(0x00);
+}
+r = cli_ctrl_e_beta(argp);
+if(!r) {
+printf("%s\n","<< Error at fn. cli_ctrl_e_beta()");
+return(0x00);
+}
+r = cli_ctrl_d_beta(argp);
+if(!r) {
+printf("%s\n","<< Error at fn. cli_ctrl_d_beta()");
+return(0x00);
+}}
+//*/
+return(0x01);
+}
 
 r = cli_ctrl_b_beta(argp);
 if(!r) {
