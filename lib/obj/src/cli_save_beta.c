9a468534f628aecd09135377d0f43546e0d27e20
Chris <chlds@yahoo.com>
2020-05-11 15:55:05 +0900

    Fix

--- lib/obj/src/cli_save_beta.c
+++ lib/obj/src/cli_save_beta.c
@@ -69,7 +69,8 @@ printf("%s\n","<< Error at fn. cli_save_as()");
 if(CLI_IRR&(R(flag,R(commandline,R(ty,*argp))))) {
 if(R(file,R(edit,R(ty,*argp)))) free(R(file,R(edit,R(ty,*argp))));
 R(file,R(edit,R(ty,*argp))) = (0x00);
-AND(R(flag,R(commandline,R(ty,*argp))),~CLI_IRR);
+flag = (~(CLI_IRR));
+AND(R(flag,R(commandline,R(ty,*argp))),flag);
 return(0x01);
 }
 return(0x00);
