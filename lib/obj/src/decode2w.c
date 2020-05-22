5895efaccf82961c7ffdb7bca506b06513befed3
Chris <chlds@yahoo.com>
2020-05-22 23:55:05 +0900

    Partially fix

--- lib/obj/src/decode2w.c
+++ lib/obj/src/decode2w.c
@@ -20,14 +20,15 @@ signed(__cdecl decode2w(signed(size),signed short(*di),signed char(*si))) {
 
 /* **** DATA, BSS and STACK */
 auto signed char *p;
-auto signed i,r;
+auto signed i,l,r,v;
+auto signed short surrog;
 auto signed short flag;
 
 /* **** CODE/TEXT */
 if(!di) return(0x00);
 if(!si) return(0x00);
 
-if(size<(0x01)) {
+if(size<(0x01+(0x01))) {
 *di = (0x00);
 return(0x00);
 }
@@ -43,11 +44,31 @@ printf("%s\n","<< Error at fn. decode2uni()");
 return(0x00);
 }
 
-si = (r+(si));
+l = (r);
+v = (0x00);
+if(0x03<(l)) {
+r = decode_surrogate_first(&surrog,i);
+if(!r) {
+printf("%s\n","<< Error at fn. decode_surrogate_first()");
+return(0x00);
+}
+*di = (surrog);
+di++;
+size = (size+(-(sizeof(*di))));
+v++;
+r = decode_surrogate_second(&surrog,i);
+if(!r) {
+printf("%s\n","<< Error at fn. decode_surrogate_second()");
+return(0x00);
+}
+i = (signed) (surrog);
+}
+
+si = (l+(si));
 *di = (signed short) (i);
 di++;
-r = (sizeof(*di));
-size = (-r+(size));
+size = (size+(-(sizeof(*di))));
+v++;
 
-return(0x01+(decode2w(size,di,si)));
+return(v+(decode2w(size,di,si)));
 }
