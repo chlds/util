13db1e8b2c0d24db050630b14342188c291d06e2
Chris <chlds@yahoo.com>
2020-07-13 18:00:05 +0900

    Fix

--- lib/obj/src/cli_ctrl_rs_beta.c
+++ lib/obj/src/cli_ctrl_rs_beta.c
@@ -2,10 +2,11 @@
 
 Press <Ctrl-\> to invoke the function.
 
-Along with C and Windows libraries
+Type a bullet (i.e., 0x2022) in UTF-8
 
 Remarks:
 Refer at util/lib/obj/src/cli_io_beta.c
+Along with C and Windows libraries
 */
 
 
@@ -19,13 +20,50 @@ Refer at util/lib/obj/src/cli_io_beta.c
 
 signed(__cdecl cli_ctrl_rs_beta(CLI_W32_STAT(*argp))) {
 
-auto signed char *p;
+auto signed char *cur,*p;
 auto signed c,i,r;
 auto signed short flag;
 
 if(!argp) return(0x00);
 
 if(CLI_DBG_D<(CLI_DBG)) printf("%s","<Ctrl-\\>");
+
+cur = (*(CLI_INDEX+(R(cur,R(ty,*argp)))));
+r = (0x01+(ct(cur)));
+r = (r*(sizeof(signed char)));
+p = (signed char(*)) malloc(r);
+if(!p) {
+printf("%s\n","<< Error at fn. malloc() ");
+return(0x00);
+}
+
+*p = (0x00);
+r = cpy(p,cur);
+
+// type a bullet in three bytes
+*cur = (0xE0|0x02);
+cur++;
+*cur = (0x80|0x00);
+cur++;
+*cur = (0x80|0x20|0x02);
+cur++;
+*cur = (0x00);
+r = cpy(cur,p);
+embed(0x00/* flag */,p);
+free(p);
+*(CLI_INDEX+(R(cur,R(ty,*argp)))) = (cur);
+
+r = cli_coord_out_beta(0x00/* wrap */,-0x03+(cur),argp);
+if(!p) {
+printf("%s\n","<< Error at fn. cli_coord_out_beta() ");
+return(0x00);
+}
+
+r = cli_refresh_beta(0x00/* forced */,cur,argp);
+if(!p) {
+printf("%s\n","<< Error at fn. cli_refresh_beta() ");
+return(0x00);
+}
 
 return(0x01);
 }
