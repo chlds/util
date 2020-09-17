d6ffc5f562ad80b45bb2a15f540cfa3dc78401e0
Chris <chlds@yahoo.com>
2020-09-17 18:25:05 +0900

    Fix

--- bin/obj/src/sar.c
+++ bin/obj/src/sar.c
@@ -19,11 +19,15 @@ auto void *process;
 auto signed *d,*process_ids;
 auto signed char *b,*cur,*base,*p;
 
-auto signed i,l,r;
+auto signed access_right;
+auto signed i,l,n,r;
 auto signed short flag;
 auto signed char c;
 
 /* **** CODE/TEXT */
+flag = (0x00);
+if(0x01<(argc)) OR(flag,OPT_VERBOSE);
+
 r = (BUFF);
 r = (r*(sizeof(signed)));
 process_ids = (signed(*)) malloc(r);
@@ -42,28 +46,41 @@ i = (i/(sizeof(signed)));
 printf("%s %s \n","_No.","___PID");
 }
 
+access_right = (PROCESS_QUERY_INFORMATION|PROCESS_VM_READ);
 d = (process_ids);
 l = (0x00);
+n = (0x00);
+
 while(i) {
 --i;
-printf("%3d. %6d ",l,*d);
-l++;
-d++;
+if(OPT_VERBOSE&(flag)) printf("%3d. %6d ",l++,*d);
+// map
+process = OpenProcess(access_right,0x00/* inherit */,*d/* pid */);
+if(!process) {
+r = GetLastError();
+if(OPT_VERBOSE&(flag)) printf("%s %d %s %Xh \n","<< Error at fn. OpenProcess() with no.",r,"or",r);
+}
+else {
+if(!(OPT_VERBOSE&(flag))) printf("%3d. %6d ",n++,*d);
+// display modules for the process id
 printf("\n");
 }
-
-/*
+if(process) {
 r = CloseHandle(process);
 if(!r) {
-printf("%s \n","<< Error at fn. CloseHandle()");
+r = GetLastError();
+printf("%s %d %s %Xh \n","<< Error at fn. CloseHandle() with no.",r,"or",r);
+break;
 // return(0x00);
+}}
+d++;
 }
-//*/
 
 free(process_ids);
 
 printf("\n");
-printf("%s %d \n","Total for processes: ",l);
+if(!(OPT_VERBOSE&(flag))) l = (n);
+printf("%s %d \n","Total for processes:",l);
 
 return(0x01);
 }
