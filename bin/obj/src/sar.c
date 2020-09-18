2e92d8290dd60c58d31f0a5122d7b93ffcf900a2
Chris <chlds@yahoo.com>
2020-09-18 16:26:55 +0900

    Fix

--- bin/obj/src/sar.c
+++ bin/obj/src/sar.c
@@ -13,14 +13,19 @@
 /* **** entry point */
 signed(__cdecl main(signed(argc),signed char(**argv),signed char(**envp))) {
 
-auto void **q,**modules;
+/* **** DATA, BSS and STACK */
+auto signed char fname[BUFF] = {
+(signed char) (0x00),
+};
+
+auto void **q,**m,**modules;
 auto void *process;
 
 auto signed *d,*process_ids;
 auto signed char *b,*cur,*base,*p;
 
 auto signed access_right;
-auto signed i,l,n,r;
+auto signed i,ii,l,n,r;
 auto signed short flag;
 auto signed char c;
 
@@ -43,7 +48,7 @@ return(0x00);
 
 if(i) {
 i = (i/(sizeof(signed)));
-printf("%s %s \n","_No.","___PID");
+printf("%3s. %6s %s (%s) \n","No","PID","BaseName","Modules");
 }
 
 access_right = (PROCESS_QUERY_INFORMATION|PROCESS_VM_READ);
@@ -61,9 +66,50 @@ r = GetLastError();
 if(OPT_VERBOSE&(flag)) printf("%s %d %s %Xh \n","<< Error at fn. OpenProcess() with no.",r,"or",r);
 }
 else {
-if(!(OPT_VERBOSE&(flag))) printf("%3d. %6d ",n++,*d);
 // display modules for the process id
+r = (BUFF);
+r = (r*(sizeof(void*)));
+modules = (void**) malloc(r);
+if(!modules) {
+printf("%s \n","<< Error at fn. malloc()");
+break;
+}
+m = (modules);
+r = EnumProcessModules(process,(void*) m,r,&ii);
+if(!r) {
+r = GetLastError();
+printf("%s %d %s %Xh \n","<< Error at fn. EnumProcessModules() with no.",r,"or",r);
+// break;
+// return(0x00);
+}
+if(r<(ii)) {
+}
+ii = (ii/(sizeof(void*)));
+r = GetModuleBaseName(process,*m,fname,BUFF);
+if(!r) {
+}
+if(!(OPT_VERBOSE&(flag))) printf("%3d. %6d ",n++,*d);
+if(fname) {
+printf("%s ",fname);
+printf("(%d) ",ii);
+//* Aux.
+if(OPT_VERBOSE&(flag)) {
 printf("\n");
+while(--ii) {
+m++;
+r = GetModuleBaseName(process,*m,fname,BUFF);
+if(!r) {
+r = GetLastError();
+printf("%s %d %s %Xh \n","<< Error at fn. GetModuleBaseName() with no.",r,"or",r);
+break;
+}
+printf("%s ",fname);
+}}
+//*/
+}
+printf("\n");
+free(modules);
+modules = (0x00);
 }
 if(process) {
 r = CloseHandle(process);
