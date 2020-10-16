341fa7d6cc885d6bb0dd419711459add17e85aa0
Chris <chlds@yahoo.com>
2020-10-16 14:36:05 +0900

    Fix

--- bin/obj/src/calend.c
+++ bin/obj/src/calend.c
@@ -12,10 +12,16 @@ Refer at <corecrt_wtime.h>
 signed(__cdecl main(signed(argc),signed char(**argv),signed char(**envp))) {
 
 /* **** DATA, BSS and STACK */
+auto signed SUNDAY = (0x00);
+auto signed WEEK = (0x07);
+
 auto struct tm *tp;
-auto time_t t;
+auto time_t tt,t;
+auto signed curr_hr,curr_mn,curr_sm;
+auto signed curr_y,curr_m,curr_d,curr_w;
+auto signed m,w;
 auto signed d,h;
-auto signed i,r;
+auto signed i,l,r;
 auto signed short flag;
 
 /* **** CODE/TEXT */
@@ -23,38 +29,100 @@ h = (60*(60));
 d = (24*(h));
 time(&t);
 tp = localtime(&t);
-if(!((signed long long) tp^(~(0x00)))) {
+if(!tp) {
 r = (errno);
-printf("%s%d%s%Xh\n","<< Error at fn. localtime() with errno. ",r," or ",r);
-printf("%s\n",strerror(r));
+printf("%s %d %s %Xh \n","<< Error at fn. localtime() with errno.",r,"or",r);
+printf("%s \n",strerror(r));
 return(0x00);
 }
 
-/* Monitoring
-printf("%s%p\n","tp is: ",tp);
-printf("%s%d%s%Xh\n","t is: ",t," or ",t);
-//*/
+curr_y = (1900+(R(tm_year,*tp)));
+curr_m = (R(tm_mon,*tp));
+curr_d = (R(tm_mday,*tp));
+curr_w = (R(tm_wday,*tp));
 
+curr_hr = (R(tm_hour,*tp));
+curr_mn = (R(tm_min,*tp));
+curr_sm = (R(tm_sec,*tp));
+
+if(DBG) {
+printf("%s %p \n","tp is:",tp);
+printf("%s %lld %s %llXh \n","t is:",t,"or",t);
+}
+
+// today 1/2
 printf("\n");
-printf("  %s %d %s %d, ",*(dayoftheweek+(R(tm_wday,*tp))),R(tm_mday,*tp),*(month+(R(tm_mon,*tp))),1900+(R(tm_year,*tp)));
+printf("\t%s %d %s %d, ",*(dayoftheweek+(R(tm_wday,*tp))),R(tm_mday,*tp),*(month+(R(tm_mon,*tp))),1900+(R(tm_year,*tp)));
 printf("%d:%02d:%02d, ",R(tm_hour,*tp),R(tm_min,*tp),R(tm_sec,*tp));
-printf("%s%d, ","Daylight Savings Time ",R(tm_isdst,*tp));
-printf("%d%s\n",R(tm_yday,*tp)," days since January 1");
+printf("%s %d, ","Daylight Savings Time",R(tm_isdst,*tp));
+printf("%d %s \n",R(tm_yday,*tp),"days since January 1");
 printf("\n");
 
-i = R(tm_mon,*tp);
-while(0x01) {
-if(i^(R(tm_mon,*tp))) break;
-printf("  %2d %s",R(tm_mday,*tp),*(dayofthewk+(R(tm_wday,*tp))));
-printf("\n");
-t = (d+(t));
-tp = localtime(&t);
-if(!((signed long long) tp^(~(0x00)))) {
+// search back for Sunday
+l = (0x00);
+tt = (t);
+while(tt) {
+if(!(SUNDAY^(R(tm_wday,*tp)))) break;
+ADD(tt,-d);
+l++;
+tp = localtime(&tt);
+if(!tp) {
 r = (errno);
-printf("%s%d%s%Xh\n","<< Error at fn. localtime() with errno. ",r," or ",r);
-printf("%s\n",strerror(r));
+printf("%s %d %s %Xh \n","<< Error at fn. localtime() with errno.",r,"or",r);
+printf("%s \n",strerror(r));
 return(0x00);
 }}
+
+flag = (0x00);
+if(!(curr_m^(R(tm_mon,*tp)))) printf("\t%s %d \n",*(month+(R(tm_mon,*tp))),curr_y);
+else flag++;
+
+printf("\t(%d %s - ",R(tm_mday,*tp),*(dayofthewk+(R(tm_wday,*tp))));
+ADD(tt,d*(-0x01+(WEEK)));
+tp = localtime(&tt);
+if(!tp) {
+r = (errno);
+printf("%s %d %s %Xh \n","<< Error at fn. localtime() with errno.",r,"or",r);
+printf("%s \n",strerror(r));
+return(0x00);
+}
+printf("%d %s) \n",R(tm_mday,*tp),*(dayofthewk+(R(tm_wday,*tp))));
+
+if(flag) printf("\t%s %d \n",*(month+(R(tm_mon,*tp))),1900+(R(tm_year,*tp)));
+
+// today 2/2
+printf("\t%d %s ",curr_d,*(dayofthewk+(curr_w)));
+printf("%d:%02d:%02d \n",curr_hr,curr_mn,curr_sm);
+
+// continue
+m = (curr_m);
+l = (0x00);
+while(0x01) {
+ADD(tt,d);
+tp = localtime(&tt);
+if(!tp) {
+r = (errno);
+printf("%s %d %s %Xh \n","<< Error at fn. localtime() with errno.",r,"or",r);
+printf("%s \n",strerror(r));
+return(0x00);
+}
+m = (R(tm_mon,*tp));
+if(curr_m^(m)) {
+if(0x02<(l++)) break;
+curr_m = (m);
+printf("\t%s %d \n",*(month+(R(tm_mon,*tp))),1900+(R(tm_year,*tp)));
+}
+printf("\t(%d %s - ",R(tm_mday,*tp),*(dayofthewk+(R(tm_wday,*tp))));
+ADD(tt,d*(-0x01+(WEEK)));
+tp = localtime(&tt);
+if(!tp) {
+r = (errno);
+printf("%s %d %s %Xh \n","<< Error at fn. localtime() with errno.",r,"or",r);
+printf("%s \n",strerror(r));
+return(0x00);
+}
+printf("%d %s) \n",R(tm_mday,*tp),*(dayofthewk+(R(tm_wday,*tp))));
+}
 
 return(0x01);
 }
