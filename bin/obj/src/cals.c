7fd5ce600978f54785a2c6241788c9442f94b098
Chris <chlds@yahoo.com>
2020-11-06 11:03:15 +0900

    Fix

--- bin/obj/src/cals.c
+++ bin/obj/src/cals.c
@@ -12,6 +12,7 @@ Refer at <corecrt_wtime.h>
 signed(__cdecl main(signed(argc),signed char(**argv),signed char(**envp))) {
 
 /* **** DATA, BSS and STACK */
+auto signed FOR_MONTHS = (0x04);
 auto signed COL_R = (0x38);
 auto signed WEEK = (0x07);
 
@@ -21,32 +22,32 @@ auto signed char *b;
 auto cals_stat_t cs;
 auto time_t curr_wk1;
 auto time_t curr_t;
-auto time_t tt,t;
+auto time_t t;
 auto signed curr_hr,curr_mn,curr_sm;
 auto signed curr_yr,curr_mo,curr_di,curr_wk;
-auto signed mm,m;
-auto signed wk,di,hr;
 auto signed i,l,n,r;
-auto signed short months;
-auto signed short jour,mois;
+auto signed short for_months;
 auto signed short flag;
 
 /* **** CODE/TEXT */
-months = (0x04);
+for_months = (FOR_MONTHS);
 if(0x01<(argc)) {
 b = (*(argv+(argc+(~0x00))));
 r = cv_da(0x0A,&i,b);
 if(!r) return(0x00);
+if(!i) i = (FOR_MONTHS);
 // if(i<(0x00)) i = (0x01+(~i));
-months = (i);
+for_months = (i);
+}
+
+AND(flag,0x00);
+if(0x01<(argc)) {
+r = cmpr_parts(&i,*(argv+(0x01)),"v");
+if(!i) OR(flag,OPT_VERBOSE);
 }
 
 r = cals_stat_init(&cs);
 if(!r) return(0x00);
-
-hr = (60*(60));
-di = (24*(hr));
-wk = (7*(di));
 
 time(&t);
 tp = localtime(&t);
@@ -63,6 +64,8 @@ printf("%s %lld %s %llXh \n","t is:",t,"or",t);
 }
 
 curr_t = (t);
+
+/*
 curr_yr = (1900+(R(tm_year,*tp)));
 curr_mo = (R(tm_mon,*tp));
 curr_di = (R(tm_mday,*tp));
@@ -70,9 +73,9 @@ curr_wk = (R(tm_wday,*tp));
 curr_hr = (R(tm_hour,*tp));
 curr_mn = (R(tm_min,*tp));
 curr_sm = (R(tm_sec,*tp));
+//*/
 
-
-/* check calendar week 1
+/* check for calendar week 1
 r = cals_retrieve_week1(*(THEFIRST+(R(day,cs))),*(THEFIRST+(R(month,cs))),&t,curr_t);
 if(!r) {
 printf("%s \n","<< Error at fn. cals_retrieve_week1()");
@@ -100,6 +103,7 @@ printf("%s %d, ","Daylight Savings Time",R(tm_isdst,*tp));
 printf("%d %s \n",R(tm_yday,*tp),"days since January 1");
 printf("\n");
 
+if(OPT_VERBOSE&(flag)) {
 // calendar week 1 of the year,
 printf("\t%s, \n","Calendar week 1 of the year");
 // update the tp for CW 1 of the year,
@@ -115,15 +119,16 @@ printf("%d:%02d:%02d, ",R(tm_hour,*tp),R(tm_min,*tp),R(tm_sec,*tp));
 printf("%s %d, ","Daylight Savings Time",R(tm_isdst,*tp));
 printf("%d %s \n",R(tm_yday,*tp),"days since January 1");
 printf("\n");
+}
 
-//* backward
-if(months<(0x00)) {
-r = cals_backward(months,&cs);
+//* backward i.e., *(CLI_BASE+(R(wk1/t, to be changed after going backward for months
+if(for_months<(0x00)) {
+r = cals_backward(for_months,&cs);
 if(!r) {
 printf("%s \n","<< Error at fn. cals_backward()");
 return(0x00);
 }
-else {
+if(OPT_VERBOSE&(flag)) {
 //* check at calendar week 1 after going backward..
 tp = localtime(CLI_BASE+(R(wk1,cs)));
 if(!tp) return(0x00);
@@ -134,21 +139,18 @@ printf("%s %d, ","Daylight Savings Time",R(tm_isdst,*tp));
 printf("%d %s ",R(tm_yday,*tp),"days since January 1");
 printf("] \n");
 printf("\n");
-}
-// *(CLI_BASE+(R(wk1/t, changed after going backward for months
-*(CLI_OFFSET+(R(wk1,cs))) = (*(CLI_BASE+(R(wk1,cs))));
-*(CLI_INDEX+(R(wk1,cs))) = (*(CLI_OFFSET+(R(wk1,cs))));
-t = (*(CLI_BASE+(R(t,cs))));
-}
-else r = (months);
+}}
+else r = (for_months);
 
 if(r<(0x00)) r = (0x01+(~r));
 
 r = cals_r(r,&cs);
 if(!r) return(0x00);
 
+if(OPT_VERBOSE&(flag)) {
 printf("\n");
 printf("\t%d %s \n",r,"weeks displayed");
+}
 
 return(0x01);
 }
