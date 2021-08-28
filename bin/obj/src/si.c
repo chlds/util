2599512cf82af3cfcec6cd2c074b33d501dd42c5
Chris <chlds@yahoo.com>
2021-08-28 20:26:05 +0900

    Fix

--- bin/obj/src/si.c
+++ bin/obj/src/si.c
@@ -12,19 +12,43 @@ Refer at <corecrt_wtime.h>
 signed(__cdecl main(signed(argc),signed char(**argv),signed char(**envp))) {
 
 auto signed char *b;
-auto signed i,r;
+auto signed i,l,r;
 auto struct _stat stats;
 auto time_t t[CLI_RULE];
-auto signed char *(st[]) = {
+auto signed char *(stat_v[]) = {
 "Created  at",
 "Modified at",
 "Accessed at",
 0x00,
 };
+auto signed char *(mode_v[]) = {
+"c",
+"d",
+"-",
+"r",
+"w",
+"x",
+0x00,
+};
+auto signed mode[] = {
+_S_IFCHR,
+_S_IFDIR,
+_S_IFREG,
+_S_IREAD,
+_S_IWRITE,
+_S_IEXEC,
+0x00,
+};
+auto signed mask[] = {
+_S_IFCHR,
+_S_IFDIR,
+_S_IFREG,
+0x00,
+};
 
 time(t);
 printf("\n");
-printf("%s \n","Today");
+printf("%s ","Current local time");
 r = cals_out_t(*t);
 if(!r) {
 printf("%s \n","<< Error at fn. cals_out_t()");
@@ -56,14 +80,30 @@ printf("%s %d \n","NLINK",R(st_nlink,stats));
 
 printf("%s %d \n","MODE ",R(st_mode,stats));
 
+AND(i,0x00);
+while(*(i+(mode_v))) {
+l = (*(i+(mode)));
+r = ord_d(mask,l);
+if(r<(ct_d(mask))) {
+if(EQ(l,_S_IFMT&(R(st_mode,stats)))) printf("%s",*(i+(mode_v)));
+}
+if(!(r<(ct_d(mask)))) {
+if(l&(R(st_mode,stats))) printf("%s",*(i+(mode_v)));
+else printf("%s","-");
+}
+i++;
+}
+
+printf("\n");
+
 i = (CLI_RULE);
 *(--i+(t)) = (0x00);
 *(--i+(t)) = (R(st_atime,stats));
 *(--i+(t)) = (R(st_mtime,stats));
 *(--i+(t)) = (R(st_ctime,stats));
 
-while(*(i+(st))) {
-printf("%s ",*(i+(st)));
+while(*(i+(stat_v))) {
+printf("%s ",*(i+(stat_v)));
 r = cals_out_t(*(i+(t)));
 printf(" (");
 printf("%lld",*(i+(t)));
