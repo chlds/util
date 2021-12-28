321585c6f7187d9ef504dd62b3185e3dd3c68af7
Chris <chlds@yahoo.com>
2021-12-28 15:26:56 +0900

    Fix

--- lib/car/obj/src/chrono_time_r.c
+++ lib/car/obj/src/chrono_time_r.c
@@ -17,6 +17,8 @@ auto signed char *b;
 auto time_t t;
 auto signed r;
 auto signed short flag;
+auto signed char *colon = (":");
+auto signed char *p = ("0");
 
 if(arg<(0x01)) return(0x00);
 if(!di) return(0x00);
@@ -25,12 +27,15 @@ if(!si) return(0x00);
 AND(flag,0x00);
 b = (0x00);
 if(!(cv_d(0x0A,&b,*(--arg+(si))))) return(0x00);
-if(!(concatenate(0x00,di,b))) OR(flag,0x01);
+if(!(0x01<(ct(b)))) {
+if(!(concat_bb(di,p))) OR(flag,0x01);
+}
+if(!(concat_bb(di,b))) OR(flag,0x01);
 rl(b);
 b = (0x00);
 if(flag) return(0x00);
 if(arg) {
-if(!(concatenate(0x00,di,":"))) return(0x00);
+if(!(concat_bb(di,colon))) return(0x00);
 }
 
 return(0x01+(chrono_time_r(arg,di,si)));
