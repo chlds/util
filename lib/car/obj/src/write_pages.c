ef5a3e736bc0bacbb99dc0c41606f14a38bba486
Chris <chlds@yahoo.com>
2022-01-20 18:35:05 +0900

    Fix

--- lib/car/obj/src/write_pages.c
+++ lib/car/obj/src/write_pages.c
@@ -5,24 +5,16 @@ Write.
 
 
 # define CAR
-# include <io.h>
 # include <stdio.h>
-# include <stdlib.h>
-# include <time.h>
-# include <fcntl.h>
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <errno.h>
 # include "../../../incl/config.h"
 
 signed(__cdecl write_pages(signed char(*di),page_t(*si))) {
 
 auto signed char *b;
-auto signed fd;
-auto signed i,r;
+auto signed r;
 auto signed short flag;
-auto struct stat stats;
-auto signed access_right = (O_BINARY|(O_WRONLY));
+auto size_t size;
+auto signed char *perm = ("wronly,binary,");
 
 if(!di) return(0x00);
 if(!si) return(0x00);
@@ -31,29 +23,14 @@ flag = (*(CLI_BASE+(R(flag,*si))));
 if(!(CLI_INIT&(flag))) return(0x00);
 
 // check the file stat.
-r = stat(di,&stats);
-if(!(r^(~0x00))) {
-if(EQ(ENOENT,errno)) printf("%s \n","<< No file..");
-else printf("%s \n","<< Error at fn. stat()");
+r = already_exist_b(&size,di);
+if(!r) {
+if(EQ(size,~0x00)) printf("%s \n","<< No file..");
+else printf("%s \n","<< Error at fn. already_exist_b()");
 return(0x00);
 }
 
-if(DBG) printf("%d%s \n",R(st_size,stats),"bytes");
+if(DBG) printf("%lld%s \n",size,"bytes");
 
-fd = op_b(di,&access_right,(void*)0x00);
-if(!(fd^(~0x00))) {
-printf("%s %Xh \n","<< Error at fn. op_b() with errno",errno);
-return(0x00);
-}
-
-r = wr_pages(fd,si);
-if(!r) printf("%s \n","<< Error at fn. wr_pages()");
-
-i = (~0x00);
-if(EQ(i,cl_b(fd))) {
-printf("%s %Xh \n","<< Error at fn. cl_b() with errno",errno);
-return(0x00);
-}
-
-return(r);
+return(xt(perm,di,si,wr_pages));
 }
