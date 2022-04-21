3ee2e455163ff39b13a68c8adffc19dae6a07dcd
Chris <chlds@yahoo.com>
2022-04-21 12:10:05 +0900

    Organise

--- lib/sat/obj/src/cmdl2_clear_r.c
+++ lib/sat/obj/src/cmdl2_clear_r.c
@@ -7,16 +7,21 @@ unsigned int(__stdcall fn(void(*argp))) for fn. _beginthreadex
 */
 
 
+# define DUAL_CONFIG_H
 # define SAT_H
 # define STDIO_H
 # define STDLIB_H
+# define CAR
+# include "./../../../incl/config.h"
 # include "./../../../config.h"
 
 signed(__cdecl cmdl2_clear_r(SAT(*argp))) {
 
 auto signed r;
 
-system("cls");
+if(!(cli_es(CTRL_T))) return(0x00);
+if(!(cli_es(CTRL_L))) return(0x00);
+
 printf("\n");
 
 printf("%s \n", "Please type --exit or press <Ctrl-C> to stop.");
