6f8ef3517bca6a71636defaa508680d2278f3077
Chris <chlds@yahoo.com>
2022-05-22 00:10:55 +0900

    Fix

--- lib/incl/calendar_d.h
+++ lib/incl/calendar_d.h
@@ -6,15 +6,3 @@ Based on UTF-8
 
 # include "./calendar_en.h"
 # include "./calendar_fr.h"
-
-signed char **(CALENDAR_WEEK_INTL[]) = {
-CALENDAR_WEEK_IN_EN,
-CALENDAR_WEEK_IN_FR,
-0x00,
-};
-
-signed char **(CALENDAR_MONTH_INTL[]) = {
-CALENDAR_MONTH_IN_EN,
-CALENDAR_MONTH_IN_FR,
-0x00,
-};
--- lib/incl/calendar_fr.h
+++ lib/incl/calendar_fr.h
@@ -4,35 +4,19 @@ Based on UTF-8
 //*/
 
 
-signed char wk_fr_0[] = ("dimanche");
-signed char wk_fr_1[] = ("lundi");
-signed char wk_fr_2[] = ("mardi");
-signed char wk_fr_3[] = ("mercredi");
-signed char wk_fr_4[] = ("jeudi");
-signed char wk_fr_5[] = ("vendredi");
-signed char wk_fr_6[] = ("samedi");
-signed char *(CALENDAR_WEEK_IN_FR[]) = {
-wk_fr_0,wk_fr_1,wk_fr_2,wk_fr_3,wk_fr_4,wk_fr_5,wk_fr_6,0x00,
+static signed char *(CALENDAR_WEEK_IN_FR[]) = {
+"dimanche","lundi","mardi","mercredi","jeudi","vendredi","samedi",0x00,
 };
 
-signed char mo_fr_0[] = ("janvier");
-signed char mo_fr_1[] = {
+static signed char fevrier[] = {
 'f',0xC3,0xA9,'v','r','i','e','r',0x00,
 };
-signed char mo_fr_2[] = ("mars");
-signed char mo_fr_3[] = ("avril");
-signed char mo_fr_4[] = ("mai");
-signed char mo_fr_5[] = ("juin");
-signed char mo_fr_6[] = ("juillet");
-signed char mo_fr_7[] = {
+static signed char aout[] = {
 'a','o',0xC3,0xBB,'t',0x00,
 };
-signed char mo_fr_8[] = ("septembre");
-signed char mo_fr_9[] = ("octobre");
-signed char mo_fr_a[] = ("novembre");
-signed char mo_fr_b[] = {
+static signed char decembre[] = {
 'd',0xC3,0xA9,'c','e','m','b','r','e',0x00,
 };
-signed char *(CALENDAR_MONTH_IN_FR[]) = {
-"",mo_fr_0,mo_fr_1,mo_fr_2,mo_fr_3,mo_fr_4,mo_fr_5,mo_fr_6,mo_fr_7,mo_fr_8,mo_fr_9,mo_fr_a,mo_fr_b,0x00,
+static signed char *(CALENDAR_MONTH_IN_FR[]) = {
+"","janvier",fevrier,"mars","avril","mai","juin","juillet",aout,"septembre","octobre","novembre",decembre,0x00,
 };
