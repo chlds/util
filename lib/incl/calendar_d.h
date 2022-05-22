2cf182e425a6bad3e930e7ada5bdf32de06a9cb6
Chris <chlds@yahoo.com>
2022-05-22 16:44:05 +0900

    Add

--- lib/incl/calendar_d.h
+++ lib/incl/calendar_d.h
@@ -4,6 +4,7 @@ Based on UTF-8
 //*/
 
 
+# include "./calendar_de.h"
 # include "./calendar_en.h"
 # include "./calendar_fr.h"
 # include "./calendar_it.h"
---
+++ lib/incl/calendar_de.h
@@ -0,0 +1,16 @@
+/* Notes
+
+Based on UTF-8
+//*/
+
+
+static signed char *(CALENDAR_WEEK_IN_DE[]) = {
+"Sonntag","Montag","Dienstag","Mittwoch","Donerstag","Freitag","Samstag",0x00,
+};
+
+static signed char marz[] = {
+'m',0xC3,0xA4,'r','z',0x00,
+};
+static signed char *(CALENDAR_MONTH_IN_DE[]) = {
+"","Januar","Februar",marz,"April","Mai","Juni","Juli","August","September","Oktober","November","Dezember",0x00,
+};
