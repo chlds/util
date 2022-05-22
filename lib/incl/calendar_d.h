ec018e53e3a4bc884c161026375df3f36086e844
Chris <chlds@yahoo.com>
2022-05-22 14:59:05 +0900

    Add

--- lib/incl/calendar_d.h
+++ lib/incl/calendar_d.h
@@ -6,3 +6,4 @@ Based on UTF-8
 
 # include "./calendar_en.h"
 # include "./calendar_fr.h"
+# include "./calendar_it.h"
---
+++ lib/incl/calendar_it.h
@@ -0,0 +1,28 @@
+/* Notes
+
+Based on UTF-8
+//*/
+
+
+static signed char lunedi[] = {
+'l','u','n','e','d',0xC3,0xAC,0x00,
+};
+static signed char martedi[] = {
+'m','a','r','t','e','d',0xC3,0xAC,0x00,
+};
+static signed char mercoledi[] = {
+'m','e','r','c','o','l','e','d',0xC3,0xAC,0x00,
+};
+static signed char giovedi[] = {
+'g','i','o','v','e','d',0xC3,0xAC,0x00,
+};
+static signed char venerdi[] = {
+'v','e','n','e','r','d',0xC3,0xAC,0x00,
+};
+static signed char *(CALENDAR_WEEK_IN_IT[]) = {
+"domenica",lunedi,martedi,mercoledi,giovedi,venerdi,"sabato",0x00,
+};
+
+static signed char *(CALENDAR_MONTH_IN_IT[]) = {
+"","gennaio","febbraio","marzo","aprile","maggio","giugno","luglio","agosto","settembre","ottobre","novembre","dicembre",0x00,
+};
