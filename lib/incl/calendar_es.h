1cc5fff84bde75802ddaffeea0eb91d180267a2a
Chris <chlds@yahoo.com>
2022-05-22 19:04:05 +0900

    Add

---
+++ lib/incl/calendar_es.h
@@ -0,0 +1,19 @@
+/* Notes
+
+Based on UTF-8
+//*/
+
+
+static signed char miercoles[] = {
+'m','i',0xC3,0xA9,'r','c','o','l','e','s',0x00,
+};
+static signed char sabado[] = {
+'s',0xC3,0xA1,'b','a','d','o',0x00,
+};
+static signed char *(CALENDAR_WEEK_IN_ES[]) = {
+"domingo","lunes","martes",miercoles,"jueves","viernes",sabado,0x00,
+};
+
+static signed char *(CALENDAR_MONTH_IN_ES[]) = {
+"","enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre",0x00,
+};
