b63b7fb66f2a2fce741393599ed270c293fab4ce
Chris <chlds@yahoo.com>
2022-05-25 16:26:05 +0900

    Add

---
+++ lib/incl/calendar_dk.h
@@ -0,0 +1,19 @@
+/* Notes
+
+Based on UTF-8
+//*/
+
+
+static signed char sondag[] = {
+'s',0xC3,0xB8,'n','d','a','g',0x00,
+};
+static signed char lordag[] = {
+'l',0xC3,0xB8,'r','d','a','g',0x00,
+};
+static signed char *(CALENDAR_WEEK_IN_DK[]) = {
+sondag,"mandag","tirsdag","onsdag","torsdag","fredag",lordag,0x00,
+};
+
+static signed char *(CALENDAR_MONTH_IN_DK[]) = {
+"","januar","februar","marts","april","maj","juni","juli","august","september","oktober","november","december",0x00,
+};
