3030ee724fa9fb0a1fb284c34fa7cdb839fdbdc9
Chris <chlds@yahoo.com>
2022-06-03 10:59:10 +0900

    Fix

--- lib/incl/calendar_en.h
+++ lib/incl/calendar_en.h
@@ -12,6 +12,13 @@ static signed char *(CALENDAR_WEEK_IN_EN[]) = {
 "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",0x00,
 };
 
+static signed char *(CALENDAR_DAY_IN_EN[]) = {
+"","1st","2nd","3rd","4th","5th","6th","7th","8th","9th",
+"10th","11th","12th","13th","14th","15th","16th","17th","18th","19th",
+"20th","21st","22nd","23rd","24th","25th","26th","27th","28th","29th",
+"30th","31st",0x00,
+};
+
 static signed char *(CALENDAR_MONTH_IN_EN[]) = {
 "","January","February","March","April","May","June","July","August","September","October","November","December",0x00,
 };
