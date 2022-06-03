db99ecd44810a5e58249bd0e70f161246f6f0b99
Chris <chlds@yahoo.com>
2022-06-04 08:40:55 +0900

    Fix

--- lib/incl/calendar_fi.h
+++ lib/incl/calendar_fi.h
@@ -12,12 +12,25 @@ static signed char *(CALENDAR_WEEK_IN_FI[]) = {
 "sunnuntai","maanantai","tiistai","keskiviikko","torstai","perjantai","lauantai",0x00,
 };
 
-static signed char kesakuu[] = {
-'k','e','s',0xC3,0xA4,'k','u','u',0x00,
+static signed char kesakuuta[] = {
+'k','e','s',0xC3,0xA4,'k','u','u','t','a',0x00,
 };
-static signed char heinakuu[] = {
-'h','e','i','n',0xC3,0xA4,'k','u','u',0x00,
+static signed char heinakuuta[] = {
+'h','e','i','n',0xC3,0xA4,'k','u','u','t','a',0x00,
 };
 static signed char *(CALENDAR_MONTH_IN_FI[]) = {
-"","tammikuu","helmikuu","maaliskuu","huhtikuu","toukokuu",kesakuu,heinakuu,"elokuu","syyskuu","lokakuu","marraskuu","joulukuu",0x00,
+"",
+"tammikuuta",
+"helmikuuta",
+"maaliskuuta",
+"huhtikuuta",
+"toukokuuta",
+kesakuuta,
+heinakuuta,
+"elokuuta",
+"syyskuuta",
+"lokakuuta",
+"marraskuuta",
+"joulukuuta",
+0x00,
 };
