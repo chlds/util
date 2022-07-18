e8bba400e47c1bd3c1c57631c96921f6fe06e56b
Chris <chlds@yahoo.com>
2022-07-18 14:46:05 +0900

    Fix

--- lib/cat/obj/src/incl/cat_d.h
+++
@@ -1,4 +0,0 @@
-struct calendar_date_time {
-signed short date[CALENDAR_DATE];
-signed short time[CALENDAR_TIME];
-} typedef calendar_date_time_t;
--- lib/cat/obj/src/incl/cat_f.h
+++ lib/cat/obj/src/incl/cat_f.h
@@ -1,4 +1,3 @@
-signed(__cdecl init_calendar_date_time(calendar_date_time_t(*argp)));
 signed(__cdecl ct_wk_mon_r(time_t(arg)));
 signed(__cdecl ct_wk_mon(time_t(arg)));
 signed(__cdecl ct_wk_sun_r(time_t(arg)));
