0476bc57237dd19b20260a61c232c08e5380f7dc
Chris <chlds@yahoo.com>
2023-01-09 18:50:55 +0900

    Fix

--- lib/incl/calendar_ca_es.h
+++ lib/incl/calendar_ca_es.h
@@ -8,43 +8,43 @@ Based on UTF-8
 
 
 static signed char *(CALENDAR_WK_IN_CA_ES[]) = {
-"dg",\
-"dl",\
-"dt",\
-"dc",\
-"dj",\
-"dv",\
-"ds",\
+"dg.",
+"dl.",
+"dt.",
+"dc.",
+"dj.",
+"dv.",
+"ds.",
 0x00,
 };
 
 static signed char *(CALENDAR_WEEK_IN_CA_ES[]) = {
-"Diumenge",\
-"Dilluns",\
-"Dimarts",\
-"Dimecres",\
-"Dijous",\
-"Divendres",\
-"Dissabte",\
+"diumenge",
+"dilluns",
+"dimarts",
+"dimecres",
+"dijous",
+"divendres",
+"dissabte",
 0x00,
 };
 
 static signed char MARCH_CA_ES[] = {
-'M','a','r',0xC3,0xA7,0x00,
+'d','e',' ','m','a','r',0xC3,0xA7,0x00,
 };
 static signed char *(CALENDAR_MONTH_IN_CA_ES[]) = {
-"",\
-"Gener",\
-"Febrer",\
+"",
+"de gener",
+"de febrer",
 MARCH_CA_ES,
-"Abril",\
-"Maig",\
-"Juny",\
-"Juliol",\
-"Agost",\
-"Setembre",\
-"Octubre",\
-"Novembre",\
-"Desembre",\
+"d'abril",
+"de maig",
+"de juny",
+"de juliol",
+"d'agost",
+"de setembre",
+"d'octubre",
+"de novembre",
+"de desembre",
 0x00,
 };
--- lib/incl/calendar_cs_cz.h
+++ lib/incl/calendar_cs_cz.h
@@ -8,31 +8,31 @@ Based on UTF-8
 
 
 static signed char SUN_CS_CZ[] = {
-'N','e',0x00,
+'n','e',0x00,
 };
 static signed char MON_CS_CZ[] = {
-'P','o',0x00,
+'p','o',0x00,
 };
 static signed char TUE_CS_CZ[] = {
-0xC3,0x9A,'t',0x00,
+0xC3,0xBA,'t',0x00,
 };
 static signed char WED_CS_CZ[] = {
-'S','t',0x00,
+'s','t',0x00,
 };
 static signed char THU_CS_CZ[] = {
-0xC4,0x8C,'t',0x00,
+0xC4,0x8D,'t',0x00,
 };
 static signed char FRI_CS_CZ[] = {
-'P',0xC3,0xA1,0x00,
+'p',0xC3,0xA1,0x00,
 };
 static signed char *(CALENDAR_WK_IN_CS_CZ[]) = {
-SUN_CS_CZ,\
-MON_CS_CZ,\
-TUE_CS_CZ,\
-WED_CS_CZ,\
-THU_CS_CZ,\
-FRI_CS_CZ,\
-"So",\
+SUN_CS_CZ,
+MON_CS_CZ,
+TUE_CS_CZ,
+WED_CS_CZ,
+THU_CS_CZ,
+FRI_CS_CZ,
+"so",
 0x00,
 };
 
@@ -55,16 +55,17 @@ static signed char FRIDAY_CS_CZ[] = {
 'p',0xC3,0xA1,'t','e','k',0x00,
 };
 static signed char *(CALENDAR_WEEK_IN_CS_CZ[]) = {
-SUNDAY_CS_CZ,\
-MONDAY_CS_CZ,\
-TUESDAY_CS_CZ,\
-WEDNESDAY_CS_CZ,\
-THURSDAY_CS_CZ,\
-FRIDAY_CS_CZ,\
-"sobota",\
+SUNDAY_CS_CZ,
+MONDAY_CS_CZ,
+TUESDAY_CS_CZ,
+WEDNESDAY_CS_CZ,
+THURSDAY_CS_CZ,
+FRIDAY_CS_CZ,
+"sobota",
 0x00,
 };
 
+/*
 static signed char FEBRUARY_CS_CZ[] = {
 0xC3,0xBA,'n','o','r',0x00,
 };
@@ -87,18 +88,58 @@ static signed char OCTOBER_CS_CZ[] = {
 0xC5,0x99,0xC3,0xAD,'j','e','n',0x00,
 };
 static signed char *(CALENDAR_MONTH_IN_CS_CZ[]) = {
-"",\
-"leden",\
-FEBRUARY_CS_CZ,\
-MARCH_CS_CZ,\
-"duben",\
-MAY_CS_CZ,\
-JUNE_CS_CZ,\
-JULY_CS_CZ,\
-"srpen",\
-SEPTEMBER_CS_CZ,\
-OCTOBER_CS_CZ,\
-"listopad",\
-"prosinec",\
+"",
+"leden",
+FEBRUARY_CS_CZ,
+MARCH_CS_CZ,
+"duben",
+MAY_CS_CZ,
+JUNE_CS_CZ,
+JULY_CS_CZ,
+"srpen",
+SEPTEMBER_CS_CZ,
+OCTOBER_CS_CZ,
+"listopad",
+"prosinec",
+0x00,
+};
+//*/
+
+// genitives
+static signed char FEBRUARY_CS_CZ[] = {
+0xC3,0xBA,'n','o','r','a',0x00,
+};
+static signed char MARCH_CS_CZ[] = {
+'b',0xC5,0x99,'e','z','n','a',0x00,
+};
+static signed char MAY_CS_CZ[] = {
+'k','v',0xC4,0x9B,'t','n','a',0x00,
+};
+static signed char JUNE_CS_CZ[] = {
+0xC4,0x8D,'e','r','v','n','a',0x00,
+};
+static signed char JULY_CS_CZ[] = {
+0xC4,0x8D,'e','r','v','e','n','c','e',0x00,
+};
+static signed char SEPTEMBER_CS_CZ[] = {
+'z',0xC3,0xA1,0xC5,0x99,0xC3,0xAD,0x00,
+};
+static signed char OCTOBER_CS_CZ[] = {
+0xC5,0x99,0xC3,0xAD,'j','n','a',0x00,
+};
+static signed char *(CALENDAR_MONTH_IN_CS_CZ[]) = {
+"",
+"ledna",
+FEBRUARY_CS_CZ,
+MARCH_CS_CZ,
+"dubna",
+MAY_CS_CZ,
+JUNE_CS_CZ,
+JULY_CS_CZ,
+"srpna",
+SEPTEMBER_CS_CZ,
+OCTOBER_CS_CZ,
+"listopadu",
+"prosince",
 0x00,
 };
