7c2d894923b16af2ade12892e6a3bce706897cf1
Chris <chlds@yahoo.com>
2023-01-10 12:03:06 +0900

    Fix

--- lib/incl/calendar_fr_fr.h
+++ lib/incl/calendar_fr_fr.h
@@ -1,14 +1,17 @@
 /* Notes
 
+French
+
+Remarks:
 Based on UTF-8
-//*/
+*/
 
 
-static signed char *(CALENDAR_WK_IN_FR[]) = {
-"Di","Lu","Ma","Me","Je","Ve","Sa",0x00,
+static signed char *(CALENDAR_WK_IN_FR_FR[]) = {
+"dim.","lun.","mar.","mer.","jeu.","ven.","sam.",0x00,
 };
 
-static signed char *(CALENDAR_WEEK_IN_FR[]) = {
+static signed char *(CALENDAR_WEEK_IN_FR_FR[]) = {
 "dimanche","lundi","mardi","mercredi","jeudi","vendredi","samedi",0x00,
 };
 
@@ -25,6 +28,6 @@ static signed char aout[] = {
 static signed char decembre[] = {
 'd',0xC3,0xA9,'c','e','m','b','r','e',0x00,
 };
-static signed char *(CALENDAR_MONTH_IN_FR[]) = {
+static signed char *(CALENDAR_MONTH_IN_FR_FR[]) = {
 "","janvier",fevrier,"mars","avril","mai","juin","juillet",aout,"septembre","octobre","novembre",decembre,0x00,
 };
--- lib/incl/calendar_nl_nl.h
+++ lib/incl/calendar_nl_nl.h
@@ -1,17 +1,20 @@
 /* Notes
 
+Dutch
+
+Remarks:
 Based on UTF-8
-//*/
+*/
 
 
-static signed char *(CALENDAR_WK_IN_NL[]) = {
-"Zo","Ma","Di","Wo","Do","Vr","Za",0x00,
+static signed char *(CALENDAR_WK_IN_NL_NL[]) = {
+"zo","ma","di","wo","do","vr","za",0x00,
 };
 
-static signed char *(CALENDAR_WEEK_IN_NL[]) = {
+static signed char *(CALENDAR_WEEK_IN_NL_NL[]) = {
 "zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag",0x00,
 };
 
-static signed char *(CALENDAR_MONTH_IN_NL[]) = {
+static signed char *(CALENDAR_MONTH_IN_NL_NL[]) = {
 "","januari","februari","maart","april","mei","juni","juli","augustus","september","oktober","november","december",0x00,
 };
