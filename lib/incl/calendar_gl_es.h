47b529ce684e0f99021ce9753007f5dd79552318
Chris <chlds@yahoo.com>
2023-01-20 16:30:03 +0900

    Fix

--- lib/incl/calendar_gl_es.h
+++ lib/incl/calendar_gl_es.h
@@ -8,55 +8,55 @@ Based on UTF-8
 
 
 static signed char WED_GL_ES[] = {
-'M',0xC3,0xA9,'r',0x00,
+'m',0xC3,0xA9,'r',0x00,
 };
 static signed char SAT_GL_ES[] = {
-'S',0xC3,0xA1,'b',0x00,
+'s',0xC3,0xA1,'b',0x00,
 };
 static signed char *(CALENDAR_WK_IN_GL_ES[]) = {
-"Dom.",\
-"Lun.",\
-"Mar.",\
+"dom.",\
+"luns",\
+"mar.",\
 WED_GL_ES,\
-"Xov.",\
-"Ven.",\
+"xov.",\
+"ven.",\
 SAT_GL_ES,\
 0x00,
 };
 
 static signed char WEDNESDAY_GL_ES[] = {
-'M',0xC3,0xA9,'r','c','o','r','e','s',0x00,
+'m',0xC3,0xA9,'r','c','o','r','e','s',0x00,
 };
 static signed char SATURDAY_GL_ES[] = {
-'S',0xC3,0xA1,'b','a','d','o',0x00,
+'s',0xC3,0xA1,'b','a','d','o',0x00,
 };
 static signed char *(CALENDAR_WEEK_IN_GL_ES[]) = {
-"Domingo",\
-"Luns",\
-"Martes",\
+"domingo",\
+"luns",\
+"martes",\
 WEDNESDAY_GL_ES,\
-"Xoves",\
-"Venres",\
+"xoves",\
+"venres",\
 SATURDAY_GL_ES,\
 0x00,
 };
 
 static signed char JUNE_GL_ES[] = {
-'X','u',0xC3,0xB1,'o',0x00,
+'x','u',0xC3,0xB1,'o',0x00,
 };
 static signed char *(CALENDAR_MONTH_IN_GL_ES[]) = {
 "",\
-"Xaneiro",\
-"Febreiro",\
-"Marzo",
-"Abril",\
-"Maio",\
+"xaneiro",\
+"febreiro",\
+"marzo",
+"abril",\
+"maio",\
 JUNE_GL_ES,
-"Xullo",\
-"Agosto",\
-"Setembro",\
-"Outubro",\
-"Novembro",\
-"Decembro",\
+"xullo",\
+"agosto",\
+"setembro",\
+"outubro",\
+"novembro",\
+"decembro",\
 0x00,
 };
