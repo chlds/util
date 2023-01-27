3b3e550d561a6388f851b9cfaeed08ec85909352
Chris <chlds@yahoo.com>
2023-01-27 15:30:56 +0900

    Add AF_ZA

---
+++ lib/incl/calendar_af_za.h
@@ -0,0 +1,47 @@
+/* Notes
+
+Afrikaans
+
+Remarks:
+Based on UTF-8
+*/
+
+
+static signed char *(CALENDAR_WK_IN_AF_ZA[]) = {
+"So.",
+"Ma.",
+"Di.",
+"Wo.",
+"Do.",
+"Vr.",
+"Sa.",
+0x00,
+};
+
+static signed char *(CALENDAR_WEEK_IN_AF_ZA[]) = {
+"Sondag",
+"Maandag",
+"Dinsdag",
+"Woensdag",
+"Donderdag",
+"Vrydag",
+"Saterdag",
+0x00,
+};
+
+static signed char *(CALENDAR_MONTH_IN_AF_ZA[]) = {
+"",
+"Januarie",
+"Februarie",
+"Maart",
+"April",
+"Mei",
+"Junie",
+"Julie",
+"Augustus",
+"September",
+"Oktober",
+"November",
+"Desember",
+0x00,
+};
