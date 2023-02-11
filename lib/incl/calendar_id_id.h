29fee1fc0eb399f0e691fb22aa0db884c18e6940
Chris <chlds@yahoo.com>
2023-02-11 18:10:00 +0900

    Add ID_ID

---
+++ lib/incl/calendar_id_id.h
@@ -0,0 +1,47 @@
+/* Notes
+
+Indonesian
+
+Remarks:
+Based on UTF-8
+*/
+
+
+static signed char *(CALENDAR_WK_IN_ID_ID[]) = {
+"Min",
+"Sen",
+"Sel",
+"Rab",
+"Kam",
+"Jum",
+"Sab",
+0x00,
+};
+
+static signed char *(CALENDAR_WEEK_IN_ID_ID[]) = {
+"Minggu",
+"Senin",
+"Selasa",
+"Rabu",
+"Kamis",
+"Jumat",
+"Sabtu",
+0x00,
+};
+
+static signed char *(CALENDAR_MONTH_IN_ID_ID[]) = {
+"",
+"Januari",
+"Februari",
+"Maret",
+"April",
+"Mei",
+"Juni",
+"Juli",
+"Agustus",
+"September",
+"Oktober",
+"November",
+"Desember",
+0x00,
+};
