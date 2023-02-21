5512447d6488a37aaf21a71ac082dc63cdfd9fa4
Chris <chlds@yahoo.com>
2023-02-21 20:40:00 +0900

    Fix

--- lib/incl/calendar_sq_al.h
+++ lib/incl/calendar_sq_al.h
@@ -7,20 +7,69 @@ Based on UTF-8
 */
 
 
-static signed char MON_SQ_AL[] = {
-'H',0xC3,0xAB,'n',0x00,
+static signed char FIRST_SQ_AL[] = {
+'e',' ','p','a','r',0xC3,0xAB,0x00,
 };
-static signed char WED_SQ_AL[] = {
-'M',0xC3,0xAB,'r',0x00,
+static signed char SECOND_SQ_AL[] = {
+'e',' ','d','y','t',0xC3,0xAB,0x00,
 };
-static signed char *(CALENDAR_WK_IN_SQ_AL[]) = {
-"Die",
-MON_SQ_AL,
-"Mar",
-WED_SQ_AL,
-"Enj",
-"Pre",
-"Sht",
+static signed char THIRD_SQ_AL[] = {
+'e',' ','t','r','e','t',0xC3,0xAB,0x00,
+};
+static signed char FOURTH_SQ_AL[] = {
+'e',' ','k','a','t',0xC3,0xAB,'r','t',0x00,
+};
+static signed char FIFTH_SQ_AL[] = {
+'e',' ','p','e','s','t',0xC3,0xAB,0x00,
+};
+static signed char AND_LAST_SQ_AL[] = {
+'d','h','e',' ','e',' ','f','u','n','d','i','t',0x00,
+};
+static signed char *(CALENDAR_ORDER_IN_SQ_AL[]) = {
+"",
+FIRST_SQ_AL,
+SECOND_SQ_AL,
+THIRD_SQ_AL,
+FOURTH_SQ_AL,
+FIFTH_SQ_AL,
+AND_LAST_SQ_AL,
+0x00,
+};
+
+static signed char ORDER_MONDAY_SQ_AL[] = {
+'e',' ','h',0xC3,0xAB,'n','a',0x00,
+};
+static signed char ORDER_WEDNESDAY_SQ_AL[] = {
+'e',' ','m',0xC3,0xAB,'r','k','u','r','a',0x00,
+};
+static signed char *(CALENDAR_ORDER_WEEK_IN_SQ_AL[]) = {
+"e diela",
+ORDER_MONDAY_SQ_AL,
+"e marta",
+ORDER_WEDNESDAY_SQ_AL,
+"e enjtja",
+"e premtja",
+"e shtuna",
+0x00,
+};
+
+static signed char ORDER_NOVEMBER_SQ_AL[] = {
+'e',' ','n',0xC3,0xAB,'n','t','o','r','i','t',0x00,
+};
+static signed char *(CALENDAR_ORDER_MONTH_IN_SQ_AL[]) = {
+"",
+"e janarit",
+"e shkurtit",
+"e marsit",
+"e prillit",
+"e majit",
+"e qershorit",
+"e korrikut",
+"e gushtit",
+"e shtatorit",
+"e tetorit",
+ORDER_NOVEMBER_SQ_AL,
+"e dhjetorit",
 0x00,
 };
 
@@ -44,6 +93,23 @@ WEDNESDAY_SQ_AL,
 "e enjte",
 "e premte",
 SATURDAY_SQ_AL,
+0x00,
+};
+
+static signed char MON_SQ_AL[] = {
+'H',0xC3,0xAB,'n',0x00,
+};
+static signed char WED_SQ_AL[] = {
+'M',0xC3,0xAB,'r',0x00,
+};
+static signed char *(CALENDAR_WK_IN_SQ_AL[]) = {
+"Die",
+MON_SQ_AL,
+"Mar",
+WED_SQ_AL,
+"Enj",
+"Pre",
+"Sht",
 0x00,
 };
 
