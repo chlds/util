32c6358429ffd1cfbb0acf66a78411e82a2c613c
Chris <chlds@yahoo.com>
2023-03-21 16:30:41 +0900

    Add SL_SL

---
+++ lib/incl/calendar_sl_sl.h
@@ -0,0 +1,118 @@
+/* Notes
+
+Slovene / Slovenian
+C4.8C/C4.8D : Latin Capital/Small Letter C with Caron
+C5.A0/C5.A1 : Latin Capital/Small Letter S with Caron
+C5.BD/C5.BE : Latin Capital/Small Letter Z with Caron
+
+Remarks:
+Based on UTF-8
+*/
+
+
+static signed char FOURTH_FEM_SL_SL[] = {
+0xC4,0x8D,'e','t','r','t','a',0x00,
+};
+static signed char *(CALENDAR_ORDER_FEMININE_IN_SL_SL[]) = {
+"",
+"prva",
+"druga",
+"tretja",
+FOURTH_FEM_SL_SL,
+"peta",
+"in zadnja",
+"v",
+0x00,
+};
+
+static signed char FOURTH_MASC_SL_SL[] = {
+0xC4,0x8D,'e','t','r','t','i',0x00,
+};
+static signed char *(CALENDAR_ORDER_MASCULINE_IN_SL_SL[]) = {
+"",
+"prvi",
+"drugi",
+"tretji",
+FOURTH_MASC_SL_SL,
+"peti",
+"in zadnji",
+"v",
+0x00,
+};
+
+static signed char FOURTH_NEUT_SL_SL[] = {
+0xC4,0x8D,'e','t','r','t','o',0x00,
+};
+static signed char *(CALENDAR_ORDER_NEUTER_IN_SL_SL[]) = {
+"",
+"prvo",
+"drugo",
+"tretje",
+FOURTH_NEUT_SL_SL,
+"peto",
+"in zadnje",
+"v",
+0x00,
+};
+
+static signed char THURSDAY_SL_SL[] = {
+0xC4,0x8D,'e','t','r','t','e','k',0x00,
+};
+static signed char *(CALENDAR_WEEK_IN_SL_SL[]) = {
+"nedelja",
+"ponedeljek",
+"torek",
+"sreda",
+THURSDAY_SL_SL,
+"petek",
+"sobota",
+0x00,
+};
+
+static signed char THU_SL_SL[] = {
+0xC4,0x8D,'e','t','.',0x00,
+};
+static signed char *(CALENDAR_WK_IN_SL_SL[]) = {
+"ned.",
+"pon.",
+"tor.",
+"sre.",
+THU_SL_SL,
+"pet.",
+"sob.",
+0x00,
+};
+
+static signed char *(CALENDAR_MONTH_ORDER_IN_SL_SL[]) = {
+"",
+"januarju",
+"februarju",
+"marcu",
+"aprilu",
+"maju",
+"juniju",
+"juliju",
+"avgustu",
+"septembru",
+"oktobru",
+"novembru",
+"decembru",
+0x00,
+};
+
+static signed char *(CALENDAR_MONTH_IN_SL_SL[]) = {
+"",
+"januar",
+"februar",
+"marec",
+"april",
+"maj",
+"junij",
+"julij",
+"avgust",
+"september",
+"oktober",
+"november",
+"december",
+0x00,
+};
