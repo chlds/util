739ec658696e8ab01f5009a3547700c3d8211fd1
Chris <chlds@yahoo.com>
2023-04-02 18:15:05 +0900

    Add TH_TH

---
+++ lib/incl/calendar_vi_vn.h
@@ -0,0 +1,104 @@
+/* Notes
+
+Vietnamese
+
+Remarks:
+Based on UTF-8
+*/
+
+
+static signed char SUNDAY_VI_VN[] = {
+'C','h',0xE1,0xBB,0xA7,' ','N','h',0xE1,0xBA,0xAD,'t',0x00,
+};
+static signed char MONDAY_VI_VN[] = {
+'T','h',0xE1,0xBB,0xA9,' ','H','a','i',0x00,
+};
+static signed char TUESDAY_VI_VN[] = {
+'T','h',0xE1,0xBB,0xA9,' ','B','a',0x00,
+};
+static signed char WEDNESDAY_VI_VN[] = {
+'T','h',0xE1,0xBB,0xA9,' ','T',0xC6,0xB0,0x00,
+};
+static signed char THURSDAY_VI_VN[] = {
+'T','h',0xE1,0xBB,0xA9,' ','N',0xC4,0x83,'m',0x00,
+};
+static signed char FRIDAY_VI_VN[] = {
+'T','h',0xE1,0xBB,0xA9,' ','S',0xC3,0xA1,'u',0x00,
+};
+static signed char SATURDAY_VI_VN[] = {
+'T','h',0xE1,0xBB,0xA9,' ','B',0xE1,0xBA,0xA3,'y',0x00,
+};
+static signed char *(CALENDAR_WEEK_IN_VI_VN[]) = {
+SUNDAY_VI_VN,
+MONDAY_VI_VN,
+TUESDAY_VI_VN,
+WEDNESDAY_VI_VN,
+THURSDAY_VI_VN,
+FRIDAY_VI_VN,
+SATURDAY_VI_VN,
+0x00,
+};
+
+static signed char *(CALENDAR_WK_IN_VI_VN[]) = {
+"CN",
+"T2",
+"T3",
+"T4",
+"T5",
+"T6",
+"T7",
+0x00,
+};
+
+static signed char JANUARY_VI_VN[] = {
+'t','h',0xC3,0xA1,'n','g',' ','1',0x00,
+};
+static signed char FEBRUARY_VI_VN[] = {
+'t','h',0xC3,0xA1,'n','g',' ','2',0x00,
+};
+static signed char MARCH_VI_VN[] = {
+'t','h',0xC3,0xA1,'n','g',' ','3',0x00,
+};
+static signed char APRIL_VI_VN[] = {
+'t','h',0xC3,0xA1,'n','g',' ','4',0x00,
+};
+static signed char MAY_VI_VN[] = {
+'t','h',0xC3,0xA1,'n','g',' ','5',0x00,
+};
+static signed char JUNE_VI_VN[] = {
+'t','h',0xC3,0xA1,'n','g',' ','6',0x00,
+};
+static signed char JULY_VI_VN[] = {
+'t','h',0xC3,0xA1,'n','g',' ','7',0x00,
+};
+static signed char AUGUST_VI_VN[] = {
+'t','h',0xC3,0xA1,'n','g',' ','8',0x00,
+};
+static signed char SEPTEMBER_VI_VN[] = {
+'t','h',0xC3,0xA1,'n','g',' ','9',0x00,
+};
+static signed char OCTOBER_VI_VN[] = {
+'t','h',0xC3,0xA1,'n','g',' ','1','0',0x00,
+};
+static signed char NOVEMBER_VI_VN[] = {
+'t','h',0xC3,0xA1,'n','g',' ','1','1',0x00,
+};
+static signed char DECEMBER_VI_VN[] = {
+'t','h',0xC3,0xA1,'n','g',' ','1','2',0x00,
+};
+static signed char *(CALENDAR_MONTH_IN_VI_VN[]) = {
+"",
+JANUARY_VI_VN,
+FEBRUARY_VI_VN,
+MARCH_VI_VN,
+APRIL_VI_VN,
+MAY_VI_VN,
+JUNE_VI_VN,
+JULY_VI_VN,
+AUGUST_VI_VN,
+SEPTEMBER_VI_VN,
+OCTOBER_VI_VN,
+NOVEMBER_VI_VN,
+DECEMBER_VI_VN,
+0x00,
+};
