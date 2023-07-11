f0ad9c07d4fb7c73f272530c3be96585acf47e12
Chris <chlds@yahoo.com>
2023-07-11 13:40:40 +0900

    Fix

--- lib/incl/calendar_fi_fi.h
+++ lib/incl/calendar_fi_fi.h
@@ -12,6 +12,23 @@ static signed char CALENDAR_LLSC_IN_FI_FI[] = {
 'F',0xC9,0xAA,0xC9,0xB4,0xC9,0xB4,0xC9,0xAA,0xEA,0x9C,0xB1,0xCA,0x9C,0x00,
 };
 
+static signed char FIRST_FI_FI[] = {
+'e','n','s','i','m','m',0xC3,0xA4,'i','n','e','n',0x00,
+};
+static signed char FOURTH_FI_FI[] = {
+'n','e','l','j',0xC3,0xA4,'s',0x00,
+};
+static signed char *(CALENDAR_ORDER_IN_FI_FI[]) = {
+"",
+FIRST_FI_FI,
+"toinen",
+"kolmas",
+FOURTH_FI_FI,
+"viides",
+"ja viimeinen",
+0x00,
+};
+
 static signed char *(CALENDAR_WK_IN_FI_FI[]) = {
 "su",
 "ma",
@@ -34,21 +51,21 @@ static signed char *(CALENDAR_WEEK_IN_FI_FI[]) = {
 0x00,
 };
 
-static signed char JUNE_FI_FI[] = {
+static signed char JUNE_PAR_FI_FI[] = {
 'k','e','s',0xC3,0xA4,'k','u','u','t','a',0x00,
 };
-static signed char JULY_FI_FI[] = {
+static signed char JULY_PAR_FI_FI[] = {
 'h','e','i','n',0xC3,0xA4,'k','u','u','t','a',0x00,
 };
-static signed char *(CALENDAR_MONTH_IN_FI_FI[]) = {
+static signed char *(CALENDAR_MONTH_PARTITIVE_IN_FI_FI[]) = {
 "",
 "tammikuuta",
 "helmikuuta",
 "maaliskuuta",
 "huhtikuuta",
 "toukokuuta",
-JUNE_FI_FI,
-JULY_FI_FI,
+JUNE_PAR_FI_FI,
+JULY_PAR_FI_FI,
 "elokuuta",
 "syyskuuta",
 "lokakuuta",
@@ -57,42 +74,48 @@ JULY_FI_FI,
 0x00,
 };
 
-static signed char FIRST_FI_FI[] = {
-'e','n','s','i','m','m',0xC3,0xA4,'i','n','e','n',0x00,
-};
-static signed char FOURTH_FI_FI[] = {
-'n','e','l','j',0xC3,0xA4,'s',0x00,
-};
-static signed char *(CALENDAR_ORDER_IN_FI_FI[]) = {
-"",
-FIRST_FI_FI,
-"toinen",
-"kolmas",
-FOURTH_FI_FI,
-"viides",
-"ja viimeinen",
-0x00,
-};
-
-static signed char ORDER_JUNE_FI_FI[] = {
+static signed char JUNE_GEN_FI_FI[] = {
 'k','e','s',0xC3,0xA4,'k','u','u','n',0x00,
 };
-static signed char ORDER_JULY_FI_FI[] = {
+static signed char JULY_GEN_FI_FI[] = {
 'h','e','i','n',0xC3,0xA4,'k','u','u','n',0x00,
 };
-static signed char *(CALENDAR_ORDER_MONTH_IN_FI_FI[]) = {
+static signed char *(CALENDAR_MONTH_GENITIVE_IN_FI_FI[]) = {
 "",
 "tammikuun",
 "helmikuun",
 "maaliskuun",
 "huhtikuun",
 "toukokuun",
-ORDER_JUNE_FI_FI,
-ORDER_JULY_FI_FI,
+JUNE_GEN_FI_FI,
+JULY_GEN_FI_FI,
 "elokuun",
 "syyskuun",
 "lokakuun",
 "marraskuun",
 "joulukuun",
+0x00,
+};
+
+static signed char JUNE_NOM_FI_FI[] = {
+'k','e','s',0xC3,0xA4,'k','u','u',0x00,
+};
+static signed char JULY_NOM_FI_FI[] = {
+'h','e','i','n',0xC3,0xA4,'k','u','u',0x00,
+};
+static signed char *(CALENDAR_MONTH_NOMINATIVE_IN_FI_FI[]) = {
+"",
+"tammikuu",
+"helmikuu",
+"maaliskuu",
+"huhtikuu",
+"toukokuu",
+JUNE_NOM_FI_FI,
+JULY_NOM_FI_FI,
+"elokuu",
+"syyskuu",
+"lokakuu",
+"marraskuu",
+"joulukuu",
 0x00,
 };
