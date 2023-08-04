b66b9e864706d3c2dd3f822cc75f67918b0640a1
Chris <chlds@yahoo.com>
2023-08-04 11:40:55 +0900

    Fix

--- lib/caw/obj/src/currently_working_process.c
+++ lib/caw/obj/src/currently_working_process.c
@@ -1,20 +1,16 @@
 /*
 
-Retrieve a pseudo handle for the current process.
+Retrieve a pseudo handle for the current process
 
 Remarks:
-The pseudo handle need not be closed when it is no longer needed.
+The pseudo handle need not be closed when it is no longer needed
 */
 
 
 # define CAW_H
-# define STDIO_H
 # define WIN32_H
 # include "./../../../config.h"
 
 void *(__cdecl currently_working_process(void)) {
-
-auto signed r;
-
 return(GetCurrentProcess());
 }
--- lib/caw/obj/src/currently_working_thread.c
+++ lib/caw/obj/src/currently_working_thread.c
@@ -1,20 +1,16 @@
 /*
 
-Retrieve a pseudo handle for the calling thread.
+Retrieve a pseudo handle for the calling thread
 
 Remarks:
-The pseudo handle need not be closed when it is no longer needed.
+The pseudo handle need not be closed when it is no longer needed
 */
 
 
 # define CAW_H
-# define STDIO_H
 # define WIN32_H
 # include "./../../../config.h"
 
 void *(__cdecl currently_working_thread(void)) {
-
-auto signed r;
-
 return(GetCurrentThread());
 }
