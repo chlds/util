c5478cf8f69a1caaede2d163aad94ce4046e0f92
Chris <chlds@yahoo.com>
2023-08-13 15:58:00 +0900

    Fix

--- lib/incl/calendar_gl_es.h
+++ lib/incl/calendar_gl_es.h
@@ -22,13 +22,13 @@ static signed char SAT_GL_ES[] = {
 's',0xC3,0xA1,'b',0x00,
 };
 static signed char *(CALENDAR_WK_IN_GL_ES[]) = {
-"dom.",\
-"luns",\
-"mar.",\
-WED_GL_ES,\
-"xov.",\
-"ven.",\
-SAT_GL_ES,\
+"dom.",
+"luns",
+"mar.",
+WED_GL_ES,
+"xov.",
+"ven.",
+SAT_GL_ES,
 0x00,
 };
 
@@ -39,13 +39,13 @@ static signed char SATURDAY_GL_ES[] = {
 's',0xC3,0xA1,'b','a','d','o',0x00,
 };
 static signed char *(CALENDAR_WEEK_IN_GL_ES[]) = {
-"domingo",\
-"luns",\
-"martes",\
-WEDNESDAY_GL_ES,\
-"xoves",\
-"venres",\
-SATURDAY_GL_ES,\
+"domingo",
+"luns",
+"martes",
+WEDNESDAY_GL_ES,
+"xoves",
+"venres",
+SATURDAY_GL_ES,
 0x00,
 };
 
@@ -53,18 +53,35 @@ static signed char JUNE_GL_ES[] = {
 'x','u',0xC3,0xB1,'o',0x00,
 };
 static signed char *(CALENDAR_MONTH_IN_GL_ES[]) = {
-"",\
-"xaneiro",\
-"febreiro",\
+"",
+"xaneiro",
+"febreiro",
 "marzo",
-"abril",\
-"maio",\
+"abril",
+"maio",
 JUNE_GL_ES,
-"xullo",\
-"agosto",\
-"setembro",\
-"outubro",\
-"novembro",\
-"decembro",\
+"xullo",
+"agosto",
+"setembro",
+"outubro",
+"novembro",
+"decembro",
+0x00,
+};
+
+static signed char *(CALENDAR_MON_IN_GL_ES[]) = {
+"",
+"xan.",
+"feb.",
+"mar.",
+"abr.",
+"maio",
+JUNE_GL_ES,
+"xul.",
+"ago.",
+"set.",
+"out.",
+"nov.",
+"dec.",
 0x00,
 };
