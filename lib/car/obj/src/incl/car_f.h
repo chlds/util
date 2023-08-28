f2175a8b1895ee2f45add701edceb2db77fd67d6
Chris <chlds@yahoo.com>
2023-08-28 21:30:48 +0900

    Add

--- lib/car/obj/src/incl/car_f.h
+++ lib/car/obj/src/incl/car_f.h
@@ -302,6 +302,7 @@ signed(__cdecl op_w(signed short(*path),signed(*argp),...));
 signed(__cdecl op_b(signed char(*path),signed(*argp),...));
 /* Operation */
 
+signed char *(__cdecl rdf_b(signed char(*argp),signed(arg)));
 signed char *(__cdecl readt_r(signed char(*argp),signed(args/* fd */),signed(argt/* letters */)));
 signed char *(__cdecl readt(signed(args/* fd */),signed(argt/* letters */)));
 signed char *(__cdecl readl_r(signed char(*argp),signed(args/* fd */),signed(argt/* times */)));
---
+++ lib/car/obj/src/rdf_b.c
@@ -0,0 +1,13 @@
+/* Notes
+
+Read letters from a file name
+*/
+
+
+# define CAR_H
+# include "./../../../config.h"
+
+signed char *(__cdecl rdf_b(signed char(*argp),signed(arg))) {
+auto signed char perm[] = ("rdonly,binary");
+return(xtt(perm,argp,arg,readt));
+}
