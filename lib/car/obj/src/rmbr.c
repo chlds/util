b6366bec73a0d33cfbe7055f6fe65ee8651e2dac
Chris <chlds@yahoo.com>
2023-10-17 20:59:25 +0900

    Fix

--- lib/car/obj/src/rmbr.c
+++ lib/car/obj/src/rmbr.c
@@ -2,12 +2,14 @@
 # include "./../../../config.h"
 
 signed char *(__cdecl rmbr(signed char(*argp))) {
+auto signed char *b;
 auto signed r;
 if(!argp) return(argp);
 if(!(*argp)) return(argp);
-r = ct(argp);
-argp = (r+(argp));
---argp;
-r = rmbr_r(r,argp);
+b = (argp);
+r = ct(b);
+b = (r+(b));
+--b;
+r = rmbr_r(r,b);
 return(argp);
 }
