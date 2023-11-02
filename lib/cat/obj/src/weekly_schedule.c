94ccd7ee8b08a4ae00a0aeab7f987d0f1213bc0c
Chris <chlds@yahoo.com>
2023-11-03 00:02:41 +0900

    Fix

--- lib/cat/obj/src/weekly_schedule.c
+++ lib/cat/obj/src/weekly_schedule.c
@@ -2,17 +2,21 @@
 # define TIME_H
 # include "./../../../config.h"
 
-signed(__cdecl weekly_schedule(time_t(*args),time_t(*argp))) {
+signed(__cdecl weekly_schedule(signed(argt),time_t(*args),time_t(*argp))) {
 auto time_t te;
-auto time_t days = (60*(60*(24*(7))));
+auto time_t day = (60*(60*(24)));
+auto time_t days = (day*(7));
 if(!args) return(0x00);
 if(!argp) return(0x00);
 te = (*argp);
-if(te<(0x00)) return(0x00);
 te = (0x01+(~te));
 te = (te+(*args));
 if(te<(0x00)) return(0x00);
-te = (te/(days));
-if(te) return(0x00);
+if(te/(days)) return(0x00);
+argt = (argt%(0x07));
+if(argt) {
+day = (day*(time_t)(argt));
+if(te/(day)) return(0x00);
+}
 return(0x01);
 }
