57f6b28fc34e42f31defdd1917a8c66d9a41360b
Chris <chlds@yahoo.com>
2023-11-05 20:20:57 +0900

    Fix

--- lib/cat/obj/src/ctwk_rr.c
+++ lib/cat/obj/src/ctwk_rr.c
@@ -6,12 +6,14 @@ signed(__cdecl ctwk_rr(time_t(args),time_t(argp))) {
 auto signed r;
 auto time_t te;
 auto time_t days = (60*(60*(24*(7))));
+auto time_t mask = (0xFFFFFFFF);
 if(!(0x00<(args))) return(0x00);
 if(!(0x00<(argp))) return(0x00);
 te = (argp);
 te = (te+(0x01+(~args)));
 if(te<(0x00)) return(0x00);
-r = (te/(days));
+te = (te/(days));
+r = (signed)(mask&(te));
 r++;
 return(r);
 }
