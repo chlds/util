dcba3ff5a2835b3d756a7e4e021fdfd1930f6938
chris <chlds@yahoo.com>
2025-02-24 14:06:40 +0900

    Fix

--- lib/cat/obj/src/cat_dies_xe.c
+++ lib/cat/obj/src/cat_dies_xe.c
@@ -3,14 +3,10 @@
 # define TIME_H
 # include "./../../../config.h"
 
-signed char *(__cdecl cat_dies_xe(signed(args),signed char(*argp))) {
+signed char *(__cdecl cat_dies_xe(signed(args/* decor */),signed char(*argp))) {
 auto signed char *b;
-auto signed r;
-auto signed char *mdot = (MDOT);
-auto signed char sp[] = (" ");
-auto signed char a[] = ("DIES");
 if(!argp) return(argp);
 b = cat_dies(args);
 if(!b) return(argp);
-return(cat_xe(b,cath(sp,mdot,sp,(void*)(0x00)),argp,(void*)(0x00)));
+return(cat_xe(b,cat_word_separator(0x01),argp,(void*)(0x00)));
 }
---
+++ lib/cat/obj/src/cat_word_separator.c
@@ -0,0 +1,13 @@
+# define CAR_H
+# define CAT_H
+# define TIME_H
+# include "./../../../config.h"
+
+signed char *(__cdecl cat_word_separator(signed(arg))) {
+auto signed char *b;
+auto signed char sp[] = (" ");
+auto signed char *mdot = (MDOT);
+b = (0x00);
+if(arg) return(cath(sp,mdot,sp,b));
+return(cath(sp,b));
+}
