e8c2521f8782724e734442471285834657223f14
chris <chlds@yahoo.com>
2025-11-06 20:26:30 +0900

    Fix

--- lib/car/obj/src/casca.c
+++ lib/car/obj/src/casca.c
@@ -2,9 +2,6 @@
 # include "./../../../config.h"
 
 signed char *(__cdecl casca(signed char *argp)) {
-auto signed char *b;
-if(!argp) return(0x00);
-b = (0x00);
-cat_b(&b,argp,(void*)0x00);
-return(casca_r(b));
+if(!argp) return(argp);
+return(casca_r(cathy(argp)));
 }
