5553365a5c5a3048cfa6cba8cefcec68430284d4
chris <chlds@yahoo.com>
2025-11-07 18:39:05 +0900

    Fix

--- lib/car/obj/src/cat_bb.c
+++ lib/car/obj/src/cat_bb.c
@@ -1,12 +1,3 @@
-/* **** Notes
-
-Concatenate
-
-Remarks:
-Call fn. rl later
-*/
-
-
 # define CAR_H
 # include "./../../../config.h"
 
@@ -16,9 +7,7 @@ auto signed r;
 if(!di) return(0x00);
 r = ct(*di);
 r++;
-r++;
-r = (r*(sizeof(**di)));
-b = (signed char(*)) alloc(r);
+b = (signed char*)(alloc(++r*(sizeof(**di))));
 if(!b) return(0x00);
 *b = (0x00);
 r = cpy(b,*di);
