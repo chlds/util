2bdb6c6ed9eabcb845c3f2432c3c30aaa2064a92
chris <chlds@yahoo.com>
2026-05-20 20:42:24 +0900

    Add

---
+++ lib/car/obj/src/lash.c
@@ -0,0 +1,8 @@
+# define CAR_H
+# include "./../../../config.h"
+
+signed(__cdecl lash(signed char(***args),signed char(*argp))) {
+if(!args) return(0x00);
+*args = cathol_xe(argp,*args);
+return(0x01);
+}
